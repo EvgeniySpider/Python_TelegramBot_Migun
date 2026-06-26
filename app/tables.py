5727a2cbc2e914609bf71c03e6dee96af3687bcf
Evgeniy <evgeniy.migun@mail.ru>
2026-06-26 22:09:36 +0200

    chore(db): связать таблицы `events` и `users` через FOREIGN KEY с каскадным удалением
    • Модернизировать DDL-запрос создания таблицы `events`, добавив CONSTRAINT `fk_events_user` для столбца `user_id`, ссылающийся на `users(telegram_id)`.
    • Настроить поведение `ON DELETE CASCADE`, гарантируя автоматическую очистку всех связанных мероприятий из таблицы `events` при удалении пользователя.
    • В seed_events.py добавить предварительный безопасный INSERT профиля пользователя через `ON CONFLICT DO NOTHING`, чтобы исключить конфликты внешнего ключа при генерации тестовой среды.

--- app/tables.py
+++ app/tables.py
@@ -17,19 +17,16 @@ CREATE_EVENTS_TABLE_QUERY = """
 CREATE TABLE IF NOT EXISTS events (
     id SERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL,
-    
-    -- 3 временных формата с жестким CHECK
     event_type VARCHAR(20) NOT NULL CHECK (event_type IN ('all_day', 'interval', 'exact')), 
-    
     title VARCHAR(255) NOT NULL,
-    
-    -- ТУТ БУДЕТ ХРАНИТЬСЯ ДОП. ИНФОРМАЦИЯ ОТ ПОЛЬЗОВАТЕЛЯ ДЛЯ СОБЫТИЯ
     description TEXT NULL, 
-    
     event_date DATE NOT NULL,
     start_time TIME NULL,
     end_time TIME NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    -- Вот она, та самая железная связь:
+    CONSTRAINT fk_events_user FOREIGN KEY (user_id) REFERENCES users(telegram_id) ON DELETE CASCADE
 );
 """
 

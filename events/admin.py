045addae58c2f5f2557045618f937b8c93d19c16
EvgeniySpider <evgeniy.migun@mail.ru>
2026-09-19 14:49:11 +0200

    Зарегистрированы модели User и Event в админ-панели

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Event, User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("telegram_id",)
+    search_fields = ("telegram_id",)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "event_date", "event_type", "user_id")
+    list_filter = ("event_type", "event_date")
+    search_fields = ("title", "description")
